6bfa09b8560b9f8a29169930e388046056d71fca
takuwind <as9214237@gmail.com>
2015-03-02 01:37:00 +0800

    Week1-HW1.1
    good look

--- HWweek1.java
+++ HWweek1.java
@@ -1,9 +1,9 @@
 class HWweek1 {
- public static void main(String[] args) {
- System.out.println("回鄉偶書  賀知章");
- System.out.println("少小離家老大回，");
- System.out.println("鄉音無改鬢毛催。");
- System.out.println("兒童相見不相識，");
- System.out.println("笑問客從何處來。");
- }
+	public static void main(String[] args) {
+		System.out.println("回鄉偶書  賀知章");
+		System.out.println("少小離家老大回，");
+		System.out.println("鄉音無改鬢毛催。");
+		System.out.println("兒童相見不相識，");
+		System.out.println("笑問客從何處來。");
+	}
 }
